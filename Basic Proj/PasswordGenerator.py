03814f13a861637368512b9fd12a6a327ff4f207
Aditya Yadav <97109361+Adityauyadav@users.noreply.github.com>
2024-03-03 01:11:41 +0530

    Created PasswordGenerator.py

---
+++ Basic Proj/PasswordGenerator.py
@@ -0,0 +1,19 @@
+#PASSWORD GENERATOR
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import random 
+letters_val = int(input("How many Letters do you want?\n"))
+numbers_val = int(input("How many numbers do you want?\n"))
+symbols_val = int(input("How many Symbols do you want?\n"))
+password = ''
+for i in range(letters_val):
+    password += random.choice(letters)
+for j in range(numbers_val):
+    password += random.choice(numbers)
+for k in range(symbols_val):
+    password += random.choice(symbols)
+
+print(password)
